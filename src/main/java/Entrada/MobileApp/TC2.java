caaa3208c2a290c4ffef945a20758e2a8225c956
Mahesh V <vmahesh17@github.appium>
2019-05-27 17:10:23 +0530

    Including TC2

---
+++ src/main/java/Entrada/MobileApp/TC2.java
@@ -0,0 +1,16 @@
+package Entrada.MobileApp;
+
+import java.net.MalformedURLException;
+
+public class TC2 extends Keywords {
+
+	// Create Job
+	public static void main(String args[]) throws MalformedURLException {
+		TC1.LoginApp();
+
+		System.out.println("Running App");
+		service.stop();
+		// click(OK);
+
+	}
+}
